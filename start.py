d3da23448e51083c117dfa0d0d13c2c6af3a6c26
Matthew Borden <borden0108@gmail.com>
2013-06-29 15:10:35 +1000

    Renamed Spec to README.md to comply with github

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from subprocess import call
 import os, time
 
-call(["pandoc", "-o", "output.pdf", "ideas.md"])
+call(["pandoc", "-o", "output.pdf", "README.md"])
 time.sleep(1)
 os.system("output.pdf")
